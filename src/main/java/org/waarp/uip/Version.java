87ff875a550a6a57fde82fe26e5a8bd452559115
Frederic Bregier <nomail@nomail.com>
2012-06-30 19:59:41 +0200

    fix 1.1.0

--- src/main/java/org/waarp/uip/Version.java
+++ src/main/java/org/waarp/uip/Version.java
@@ -1,9 +1,9 @@
 // DO NOT MODIFY - WILL BE OVERWRITTEN DURING THE BUILD PROCESS
 package org.waarp.uip;
-/** Provides the version information of GoldenGate Password. */
+/** Provides the version information of Waarp Password. */
 public final class Version {
  /** The version identifier. */
- public static final String ID = "1.0.4";
+ public static final String ID = "1.1.0";
  /** Prints out the version identifier to stdout. */
  public static void main(String[] args) { System.out.println(ID); }
  private Version() { super(); }
